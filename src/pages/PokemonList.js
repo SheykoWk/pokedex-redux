025ab46e133d09469333aaaba11f3dea845a3553
sheyko <sheyko.wk@gmail.com>
2022-05-05 22:00:17 -0500

    add prev pagination button

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -1,28 +1,42 @@
-import { useEffect, useState } from "react"
-import PokeItem from "../components/PokeItem"
-import getAllPokemons from "../services/getAllPokemons"
+import { useEffect, useState } from "react";
+import PokeItem from "../components/PokeItem";
+import getAllPokemons from "../services/getAllPokemons";
 
 const PokemonList = () => {
+  const [pokeList, setPokeList] = useState([]);
+  const [arrOffsetPosition, setArrOffsetPosition] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pages, setPages] = useState([]);
 
-    const [pokeList, setPokeList] = useState([])
-    const [arrOffsetPosition, setArrOffsetPosition] = useState(0)
+  useEffect(() => {
+    getAllPokemons(arrOffsetPosition).then((res) => {
+      setPokeList(res.data.results);
+    });
+  }, [arrOffsetPosition]);
 
+  const list = pokeList.map((item) => (
+    <PokeItem key={item.url} url={item.url} />
+  ));
 
-    useEffect(() => {
-        getAllPokemons(arrOffsetPosition)
-            .then((res) => {
-                setPokeList(res.data.results)
-            })
-    }, [arrOffsetPosition])
+  return (
+    <div>
+      {list}
+      {arrOffsetPosition === 0 ? (
+        <button onClick={() => setArrOffsetPosition(arrOffsetPosition + 20)}>
+          Next 20
+        </button>
+      ) : (
+        <>
+          <button onClick={() => setArrOffsetPosition(arrOffsetPosition + 20)}>
+            Next 20
+          </button>
+          <button onClick={() => setArrOffsetPosition(arrOffsetPosition - 20)}>
+            Prev 20
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
 
-    const list = pokeList.map((item) => <PokeItem key={item.url} url={item.url} />)
-
-    return (
-        <div>
-            {list}
-            <button onClick={() => setArrOffsetPosition(arrOffsetPosition + 20)}>Next 20</button>
-        </div>
-    )
-}
-
-export default PokemonList
+export default PokemonList;
